75a1f9c3364608ac086fe7db2d1db81f3e2e4427
Ayodele Emmanuel <69075669+Bartenderr@users.noreply.github.com>
2021-12-05 23:11:52 +0100

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,14 @@
+name = input("what is your is name? ")
+weight_kg = input("what is your weight in kg? ")
+height_m = input("what is your height in meters? ")
+height_m_sqr = float(height_m) * float(height_m)
+bmi = int(weight_kg) / float(height_m_sqr)
+roundBMI = round(bmi, 1)
+print(roundBMI)
+if bmi < 25:
+    print(name + " is not overweight")
+elif bmi == 25:
+    print(name + " is borderline")
+else:
+    if bmi > 25:
+        print(name + " is overweight")
